bacd09ca974dab7d9728f67d341e75fd4a0a9e84
Luis Ortiz <luis.ortiz.uriarte@gmail.com>
2025-01-16 10:09:11 -0500

    Update cop.py
    Fix bug in chow et al method, refactor class methods.

--- cop.py
+++ cop.py
@@ -5,48 +5,63 @@ import numpy as np
 
 class COP:
 
-    def __init__(self, set_point=296.15, unit='K'):
+    def __init__(self, set_point=296.15, unit='K', g=0.321):
+        """ Initialize the COP object with the set point temperature
+            and the unit of temperature.
+        """
 
-        # Set point temperature is 74 degF as default.
+        # Set point temperature is 23 degC as default.
         # May be changed 
         self.set_point = set_point
         self.unit = 'K'
+        self.g = g
         
         print('Set point temperature is: ' + str(self.set_point) + self.unit)
         
         return None
-
-
-    def carnot(self, outdoor_temp):
-        return self.set_point/(outdoor_temp - self.set_point)
-
-
-    def chow_2000(self, outdoor_temp):
-        print(self.set_point - 273.15) 
-        if self.set_point - 273.15 == 23:
-            a = 4.825
-            b = .0687
-        elif self.set_point - 273.15 == 25:
-            a = 5.153
-            b = .0738
-        elif self.set_point - 273.15 == 27:
-            a = 5.241
-            b = .0742
-        else:
-            print('Invalid setpoint for Chow et al (2000)')
-
-            a = np.nan
-            b = np.nan
-
-        return a + b*outdoor_temp
-
     
-    def choi(self, outdoor_temp):
-        coeff = (638.95 - 4.238*outdoor_temp)/(100 + 3.534*outdoor_temp)
+    def getCOP(self, outdoor_temp, method='carnot'):
+        """ Compute the Coefficient of Performance for real systems 
+            using a range of methods.
+        """
 
-        return coeff
+        if method == 'carnot':
+            return 0.321*self.set_point/(outdoor_temp - self.set_point)
+        
+        elif method == 'chow': 
+            if self.set_point - 273.15 == 23:
+                a = 4.825
+                b = .0687
+            elif self.set_point - 273.15 == 25:
+                a = 5.153
+                b = .0738
+            elif self.set_point - 273.15 == 27:
+                a = 5.241
+                b = .0742
+            else:
+                print('Invalid setpoint for Chow et al (2000)')
 
-    def ryu(self, outdoor_temp):
-        setpoint_c = self.set_point - 273.15
+                a = np.nan
+                b = np.nan
 
-        return 12 - 0.35*outdoor_temp + .0034*outdoor_temp**2
+            return a - b*(outdoor_temp-273.15)
+        
+        elif method == 'choi':
+            outdoor_temp = outdoor_temp-273.15
+            coeff = (638.95 - 4.238*outdoor_temp)/(100 + 3.534*outdoor_temp)
+
+            return coeff
+        
+        elif method == 'ryu':
+            setpoint_c = self.set_point - 273.15
+            outdoor_temp = outdoor_temp - 273.15
+
+            return 12 - 0.35*outdoor_temp + .0034*outdoor_temp**2
+        
+        elif method == 'exergetic':
+            coeff = self.g*self.set_point/(outdoor_temp - self.set_point)
+            return coeff
+        
+        else:
+            raise Exception(f'Defined method {method} was not found!')
+
